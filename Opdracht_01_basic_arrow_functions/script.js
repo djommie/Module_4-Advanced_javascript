640e53911d074eca447ed15518027e83e1c4b610
Jimmy Falke <jim_falke@hotmail.com>
2021-05-20 12:11:45 +0200

    first commit

---
+++ Opdracht_01_basic_arrow_functions/script.js
@@ -0,0 +1,24 @@
+// Deel A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions()
+
+// Deel B
+const fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+// Deel C
+const myFunction = (a, b) => a + b;
+
+console.log(myFunction(3, 4));
+
+// Deel D
+const addFive = (a) => a + 5;
+
+console.log(addFive(4));
+
+// Deel E
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(createObject());
